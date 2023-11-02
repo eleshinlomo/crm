2ebeade270447af6e0050f0bc1c46dc75a873f44
eleshinlomo <seun.olatunji2@gmail.com>
2023-11-02 09:29:01 -0400

    added roles

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,14 +2,26 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import Link from 'next/link'
 import { NewNavBar } from '@/components/newnavbar'
+import { ChatbotTeamPage } from '@/components/chatbotteam'
+import { Footer } from '@/components/footer'
 
 const page = () => {
   return (
-    <div className='text-center'>
+
+    <div>
+
+    <div className=' py-4 px-4'>
     
+       <div className='flex flex-col justify-center items-center'>
       <Button>
        <Link href='/dashboard'>Go to Dashboard</Link>
       </Button>
+      </div>
+
+      <ChatbotTeamPage />
+
+    </div>
+    <Footer />
 
     </div>
   )
---
+++ components/chatbotteam.tsx
@@ -0,0 +1,89 @@
+import Image from 'next/image'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+
+export const ChatbotTeamPage = ()=>{
+
+    const Chatbots = [
+
+        {
+            chatbotname: "Teema",
+            role: 'General Questions',
+            info: "Answers general questions on all topis",
+            image: '/images/girl1.png',
+            route: 'https://fixupe.com/general'
+        },
+
+        {
+            chatbotname: "Enoch",
+            role: '(Programmer)Writes code...',
+            info: "Answers general questions on all topis",
+            image: '/images/coder.png',
+            route: 'https://fixupe.com/code'
+        },
+
+        {
+            chatbotname: "Eleshin",
+            role: 'Image Generator',
+            info: "Answers general questions on all topis",
+            image: '/images/image_guy.png',
+            route: 'https://fixupe.com/image'
+        },
+
+        {
+            chatbotname: "Emerald",
+            role: 'AI Girlfriend',
+            info: "You intimate AI excapades",
+            image: '/images/girl2.png',
+            route: 'https://fixupe.com/girlfriend'
+        },
+        
+       
+    ]
+    return (
+        <div>
+
+
+<div className='py-4 text-center '>
+    <p className='leading-8 py-2 text-2xl font-extrabold'>MEET OUR AI TEAM</p>
+    <p>All images are AI generated and our team does not exist in real-life</p>
+    
+</div>
+
+<div className='flex flex-col md:flex-row lg:flex-row
+         py-2 px-5 gap-2 justify-center items-center bg-white'>
+            
+          {
+            Chatbots.map((chatbot, index)=>
+
+            <div key={index}>
+         <div className='h-72 w-72 lg:w-72 lg:72 relative'>
+            <Image src={chatbot.image} alt='member profile' fill  />  
+            </div>
+            <div className='flex flex-col justify-center items-center gap-2'>
+            <p className='text-l text-black'>Name: {chatbot.chatbotname}</p>
+            <p className='text-l text-black'>Role: {chatbot.role}</p>
+            {/* <p className='text-l text-black'>Project Info: {team.info}</p>  */}
+            <Button>
+                <Link href={chatbot.route}>CHAT</Link>
+            </Button>
+
+
+
+            </div>
+
+            
+            </div>
+            
+    
+            )
+         } 
+
+         
+          
+
+         </div>
+        </div>
+    )
+}
