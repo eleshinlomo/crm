79190ba96ddc6a09df059faaa27106371e6034f3
eleshinlomo <seun.olatunji2@gmail.com>
2024-02-18 12:54:15 -0500

    mmm

--- app/dashboard/(writing bots)/aitranscriber/page.tsx
+++ app/dashboard/(writing bots)/aitranscriber/page.tsx
@@ -78,7 +78,7 @@ const VoiceToTextPage = () => {
            
            <div>
             <p className='py-4'>Don&apos;t have an Audio file?</p>
-          <Link href='/voicerecorder'>
+          <Link href='/dashboard/voicerecorder'>
             <Button className=' text-white p-2 rounded-2xl'>
             Use Voice Recorder
           </Button>
