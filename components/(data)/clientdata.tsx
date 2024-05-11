47eca96c3bfed2d1f60747b3c578ef9cec0f5f70
eleshinlomo <seun.olatunji2@gmail.com>
2024-05-11 14:48:51 -0400

    mmm

--- components/(data)/clientdata.tsx
+++ components/(data)/clientdata.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, {useEffect, useState} from "react"
+import * as React from "react"
 import {
   ColumnDef,
   ColumnFiltersState,
@@ -12,7 +12,6 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-  Row,
 } from "@tanstack/react-table"
 import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
 
@@ -36,275 +35,255 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-fetchClientsData
-import { fetchClientsData } from "./clientfunctions"
-import { deleteClient } from "./clientfunctions"
-import { modifyClient } from "./clientfunctions"
-import CRMNavBar from "@/app/(allroutes)/crmpages/crmpage/crmnavbar"
 
+
+
+const data: Client[] = [
+
+  {
+    id: "5kma53ae",
+    company: 'Exxon',
+    contact: 'testing',
+    email: 'example@e.com',
+    mobile: '900099',
+    phone: '9595958',
+    followup: 'not contacted',
+    address: '1234 boyle street',
+    servicefee: '10%',
+    contractdoc: 'doc',
+    status: 'signed-contract',
+    amount: 80000
+    
+  },
+
+  {
+    id: "5kma53ae",
+    company: 'Exxon',
+    contact: 'testing',
+    email: 'example@e.com',
+    mobile: '900099',
+    phone: '9595958',
+    followup: 'not contacted',
+    address: '1234 boyle street',
+    servicefee: '10%',
+    contractdoc: 'doc',
+    status: 'signed-contract',
+    amount: 100000
+    
+  },
+  
+  {
+    id: "2",
+    company: 'Facebook',
+    contact: 'testing',
+    email: 'example@facebook.com',
+    mobile: '900099',
+    phone: '9595958',
+    followup: 'not contacted',
+    address: '1234 San Francisco',
+    servicefee: '10%',
+    contractdoc: 'doc',
+    status: 'in-talks',
+    amount: 10000
+  },
+  {
+    id: "5kma53ae",
+    company: 'Google',
+    contact: 'testing',
+    email: 'example@google.com',
+    mobile: '900099',
+    phone: '9595958',
+    followup: 'not contacted',
+    address: '1234 boyle street',
+    servicefee: '10%',
+    contractdoc: 'doc',
+    status: 'ongoing-contract',
+    amount: 40000
+    
+  },
+]
 
 export type Client = {
-  id: string
-  company: string
-  contact: string
-  mobile: string
-  phone: string
-  followup: string
-  address: string
-  servicefee: string
+
+  id: string,
+  company: string,
+  contact: string,
+  email: string,
+  mobile: string,
+  phone: string,
+  followup: string,
+  address: string,
+  servicefee: string,
   contractdoc: string
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
+  status: "pending" | "in-talks" | "signed-contract" | "ongoing-contract",
+  amount: number
+  
 }
 
+// Columns
 
+export const columns: ColumnDef<Client>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("status")}</div>
+    ),
+  },
 
+  {
+    accessorKey: "company",
+    header: "Company",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("company")}</div>,
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    }
+  },
+  {
+    accessorKey: "mobile",
+    header: "mobile",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("mobile")}</div>,
+  },
+  {
+    accessorKey: "phone",
+    header: "phone",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("phone")}</div>,
+  },
+  {
+    accessorKey: "followup",
+    header: "followup",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("followup")}</div>,
+  },
+  {
+    accessorKey: "address",
+    header: "address",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("address")}</div>,
+  },
+  {
+    accessorKey: "servicefee",
+    header: "servicefee",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("company")}</div>,
+  },
+  // {
+  //   accessorKey: "contractdoc",
+  //   header: "Contract",
+  //   cell: ({ row }) => <div className="capitalize">{row.getValue("contractdoc")}</div>,
+  // },
+  // {
+  //   accessorKey: "amount",
+  //   header: () => <div className="text-right">Amount</div>,
+  //   cell: ({ row }) => {
+  //     const amount = parseFloat(row.getValue("amount"))
 
+  //     // Format the amount as a dollar amount
+  //     const formatted = new Intl.NumberFormat("en-US", {
+  //       style: "currency",
+  //       currency: "USD",
+  //     }).format(amount)
 
+  //     return <div className="text-right font-medium">{formatted}</div>
+  //   },
+  // },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const client = row.original
 
-
-
-  
-  // Define your component
-  export function ClientData() {
-    const [sorting, setSorting] = React.useState<SortingState>([]);
-    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
-    const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
-    const [rowSelection, setRowSelection] = React.useState({});
-    const [message, setMessage] = useState<string>('');
-    const [clients, setClients] = useState<string>('');
-    const [company, setCompany] = useState<string>('');
-    const [isEditing, setIsEditing] = useState<boolean>(false);
-    const [clientFetched, setClientFetched] = useState<boolean>(false);
-    const [editedData, setEditedData] = useState<null | any>(null);
-    const [data, setData] = React.useState<Client[]>([]);
-
-
-
-    const handleModifyClick = (client: Client) => {
-      // Set the found client as the edited data
-      setEditedData({ ...client });
-      // Set editing state to true
-      setIsEditing(true);
-    };
-    
-
-// Function to handle onChange event in input fields
-// const handleInputChange = (e:any, key: keyof Client, value: string) => {
-//   // Update the edited data with the new value
-//   if (editedData) {
-//     setEditedData(e.target.value);
-// };
-
-
-
-
-    const columns: ColumnDef<Client>[] = [
-      {
-        id: "select",
-        header: ({ table }) => (
-          <Checkbox
-            checked={
-              table.getIsAllPageRowsSelected() ||
-              (table.getIsSomePageRowsSelected() && "indeterminate")
-            }
-            onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-            aria-label="Select all"
-          />
-        ),
-        cell: ({ row }) => (
-          <Checkbox
-            checked={row.getIsSelected()}
-            onCheckedChange={(value) => row.toggleSelected(!!value)}
-            aria-label="Select row"
-          />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-      },
-      {
-        accessorKey: "id",
-        header: () => <div className="text-right">ID</div>,
-        cell: ({ row }) => <div className="lowercase">{row.getValue("id")}</div>,
-      },
-      {
-        accessorKey: company,
-        header: () => <div className="text-right">Company</div>,
-        cell: ({ row }) => {
-          const cellData: any = row.original;
-          return isEditing ? (
-            <Input
-              value={cellData}
-              onChange={(event) => setEditedData(event.target.value)}
-            />
-          ) : (
-            <div className="lowercase">{cellData}</div>
-          );
-        },
-      },
-      
-      {
-        accessorKey: "contact",
-        header: () => <div className="text-right">Contact</div>,
-        cell: ({ row }) => <div className="lowercase">{row.getValue("contact")}</div>,
-      },
-      
-      {
-        accessorKey: "email",
-        id: "emailCol",
-        header: ({ column }) => {
-          return (
-            <Button
-              variant="ghost"
-              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-            >
-              Email
-              <ArrowUpDown className="ml-2 h-4 w-4" />
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
             </Button>
-          )
-        },
-        cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-      },
-      {
-        accessorKey: "mobile",
-        header: () => <div className="text-right">Mobile</div>,
-        cell: ({ row }) => <div className="lowercase">{row.getValue("mobile")}</div>,
-      },
-      {
-        accessorKey: "phone",
-        header: () => <div className="text-right">Phone</div>,
-        cell: ({ row }) => <div className="lowercase">{row.getValue("phone")}</div>,
-      },
-      {
-        accessorKey: "followup",
-        header: () => <div className="text-right">Follow up</div>,
-        cell: ({ row }) => <div className="lowercase">{row.getValue("followup")}</div>,
-      },
-      {
-        accessorKey: "address",
-        header: () => <div className="text-right">Address</div>,
-        cell: ({ row }) => <div className="lowercase">{row.getValue("address")}</div>,
-      },
-      {
-        accessorKey: "servicefee",
-        header: () => <div className="text-right">Service Fee</div>,
-        cell: ({ row }) => <div className="lowercase">{row.getValue("servicefee")}</div>,
-      },
-      {
-        accessorKey: "contractdoc",
-        header: () => <div className="text-right">Contract Doc</div>,
-        cell: ({ row }) => <div className="lowercase">{row.getValue("contractdoc")}</div>,
-      },
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(client.id)}
+            >
+              Copy Client ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Modify Client</DropdownMenuItem>
+            <DropdownMenuItem>Delete Client</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
 
-      {
-        id: "actions",
-        enableHiding: false,
-        cell: ({ row } : { row: Row<Client> }) => {
-          const client = row.original;
-    
-    
-          return (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 p-0">
-                  <span className="sr-only">Open menu</span>
-                  <MoreHorizontal className="h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                {/* Modify */}
-                
-          <DropdownMenuItem onClick={() => setIsEditing(true)}>
-            Modify
-          </DropdownMenuItem>
+export function ClientData() {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  )
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
 
-                <DropdownMenuSeparator />
-                <DropdownMenuItem 
-                onClick={(async () => await deleteClient(client.id))}
-                >Delete</DropdownMenuItem>
-                <DropdownMenuItem>Email</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )
-        },
-      }
-      
-    ]
-    
- 
-    
-    const fetchData = async (): Promise<Client[]>=>{
-        
-        // Get clients Handler
-           const response = await fetchClientsData(clientFetched)
-           console.log(response)
-           const clientData = response.data
-           if (clientData && clientData.length > 0){
-            setData(clientData)
-            setClients(clientData)
-            setClientFetched(true)
-          }else{
-            setMessage(response.message)
-          }
-           return response
-          }
-  
-    // Fetch the data outside the component
-    
-    useEffect(() => {
-      fetchData()
-
-      }, []);
-
-    
-    
-  
-    // Use the fetched data with useReactTable
-    const table = useReactTable({
-      data,
-      columns,
-      onSortingChange: setSorting,
-      onColumnFiltersChange: setColumnFilters,
-      getCoreRowModel: getCoreRowModel(),
-      getPaginationRowModel: getPaginationRowModel(),
-      getSortedRowModel: getSortedRowModel(),
-      getFilteredRowModel: getFilteredRowModel(),
-      onColumnVisibilityChange: setColumnVisibility,
-      onRowSelectionChange: setRowSelection,
-      state: {
-        sorting,
-        columnFilters,
-        columnVisibility,
-        rowSelection,
-      },
-    });
-  
-    
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  })
 
   return (
     <div className="w-full">
-    <div className="pt-4">
-    <CRMNavBar />
-    </div>
-   {/* Heading */}
-    <div>
-   <p className="text-center bg-clip-text text-2xl py-4 font-extrabold">
-     BUSINESS DEVELOPMENT PAGE</p>
-   <div className="text-center bg-clip-text text-xl py-1 font-extrabold">
-    {clients && clients.length > 0 ? 
-    <p>Total Clients:
-    {clients.length}</p>
-    :null
-    }</div>
-    </div>
-
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          placeholder="Filter company..."
+          value={(table.getColumn("company")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
+            table.getColumn("company")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
@@ -376,9 +355,9 @@ export type Client = {
               <TableRow>
                 <TableCell
                   colSpan={columns.length}
-                  className="h-24 text-start"
+                  className="h-24 text-center"
                 >
-                  {message}
+                  No results.
                 </TableCell>
               </TableRow>
             )}
