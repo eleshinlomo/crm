3f581a52976b2d4ad4d482605fb1eba0355a5492
eleshinlomo <seun.olatunji2@gmail.com>
2024-06-05 09:43:44 -0400

    mmm

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-    title: 'Home - Simple',
-    description: 'Page description',
+    title: 'Home - CRM',
+    description: 'CRM tools powered by AI',
   }
   
 import Hero from '@/components/hero'
